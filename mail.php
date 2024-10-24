deaf1a51eec0a7d6fd43694d6419dad8ea97da10
ChyoRi <chyori_@naver.com>
2024-10-24 23:59:56 +0900

    UpDate

---
+++ mail.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>메일전송완료</title>
+</head>
+<body>
+
+    <?php
+        $Name=$_POST['name'];
+        $Contact=$_POST['contact'];
+        $Contents =$_POST['content'];
+
+        $to='chyori_@naver.com';
+        // $subject=$Title;
+        $msg="보낸사람:$Name\n"."연락처:$Contact\n"."메일내용:$Contents";
+
+        $sendmail = mail($to,$Title,$msg);
+    ?>
+    
+
+<script>
+    alert('메일이 성공적으로 전송되었습니다.');
+    window.history.back();
+</script> 
+</body>
+</html>
+
+
