a39a9bd1a8e1fe0cc2b03764a849d8ea1be95cd1
ChyoRi <chyori_@naver.com>
2024-10-29 01:11:31 +0900

    UpDate

--- js/index.js
+++ js/index.js
@@ -14,14 +14,23 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// 요소 Selector
 const pcHeader = document.querySelector('.pc_header');
 const pcHeaderLogoImg = document.querySelector('#logo > a > img');
 const mobileNavDim = document.querySelector('.mobile_nav_dim');
 const mobileNav = document.querySelector('.mobile_nav');
 const mobileNavCloseBtn = document.querySelector('.mobile_nav_close_btn');
 const hambergerBtn = document.querySelector('.hamberger_btn');
+const mainTextBox = document.querySelector('.text_box');
+const mainHangleText = document.querySelector('.hangle_text');
+const mainEnglishText = document.querySelector('.english_text');
 const topBtnWrap = document.querySelector('.top_btn_wrap');
 const topBtn = document.querySelector('.top_btn')
+
+// mobile 해상도로 진입시 mainvideo의 한글텍스트와 영문텍스트 순서를 변경
+if(window.innerWidth <= 991) {
+  mainTextBox.insertBefore(mainEnglishText, mainHangleText);
+}
 
 // 스크롤시 상단 헤더 고정
 const pcHeaderFixed = () => {
@@ -51,28 +60,44 @@ const goTop = e => {
   window.scrollTo({top: 0});
 }
 
-// 모바일 햄버거 버튼 누를시 네비 active
+// 모바일 햄버거 버튼 누를시 네비와 dim의 active class를 추가
 const mobileNavMenu = () => {
   mobileNavDim.classList.add('active');
-  mobileNav.classList.add('active');
+  setTimeout(() => {
+    mobileNav.classList.add('active');
+  });
 }
 
-// 모바일 X버튼 누를시 네비 default
+// 모바일 X버튼 누를시 nav의 active class를 제거
 const mobileNavClose = () => {
-  mobileNavDim.classList.remove('active');
   mobileNav.classList.remove('active');
 }
 
-// resize시 PC Size로 넘어갔을경우 네비 default
+// Resize Window Event
 const resizeNavClose = () => {
+  // // resize시 PC Size로 넘어갔을경우 nav와 dim의 active class를 제거
   if(window.innerWidth >= 992) {
     mobileNavDim.classList.remove('active');
     mobileNav.classList.remove('active');
   }
+
+  // mobile 해상도로 진입시 mainvideo의 한글텍스트와 영문텍스트 순서를 변경
+  if(window.innerWidth <= 991) {
+    mainTextBox.insertBefore(mainEnglishText, mainHangleText);
+  }
 }
 
+// nav의 transion이 끝난이후에 dim의 active class를 제거
+const mobileNavCloseMove = () => {
+  if (!mobileNav.classList.contains('active')) {
+    mobileNavDim.classList.remove('active');
+  }
+}
+
+// Event Listener
 window.addEventListener('scroll', pcHeaderFixed);
 window.addEventListener('resize', resizeNavClose);
 hambergerBtn.addEventListener('click', mobileNavMenu);
 mobileNavCloseBtn.addEventListener('click', mobileNavClose);
 topBtn.addEventListener('click', goTop);
+mobileNav.addEventListener('transitionend', mobileNavCloseMove);
